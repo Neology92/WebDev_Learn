3ee272431edb9a05b45770cbfac6dbf0eef48139
Neology92 <oskarlegner@gmail.com>
2019-04-19 15:51:16 +0200

    organized as OOP and created Player and (prototype) AI control

--- pongGame/game.js
+++ pongGame/game.js
@@ -8,32 +8,34 @@ const cw = canvasElem.width;
 const ch = canvasElem.height; 
 
 
-let ball = {
-    size:20,
-    X:cw/2,
-    Y:ch/2,
-    speedX: 3,
-    speedY: 3
-}
-
-let paddle = {
-    height:100,
-    width:10
-}
+let ball;
+let pad1;
+let pad2;
 
 
-function drawPaddle()
+function setup()
 {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(pad.X,pad.Y,paddle.height,paddle.width);
+    ball = new Ball();
+    pad1 = new Paddle(70);
+    pad2 = new Paddle(910);
+
+    window.addEventListener("mousemove", function(e){ pad1.movePlayer(e);});
 }
 
 
-function drawBall()
-{
-    ctx.fillStyle = 'white';
-    ctx.fillRect(ball.X-ball.size/2,ball.Y-ball.size/2,ball.size,ball.size);
+function gameLoop() 
+{ 
+    drawTable();
+
+    pad1.draw();
+
+    pad2.moveAI();
+    pad2.draw();
+
+    ball.move();
+    ball.draw();
 }
+
 
 function drawTable()
 {
@@ -42,25 +44,82 @@ function drawTable()
 
     ctx.fillStyle = '#333333';
 
-    let lineH = 20;
-    let lineW = 6;
-    let space = 20;
+    let lineH = 15;
+    let lineW = 8;
+    let space = 25;
 
-    for(let pos=0; (pos+2*space)<ch; pos+=(space+lineH))
+    for(let posY=space; (posY+space)<ch; posY+=(space+lineH))
     {
-        ctx.fillRect((cw-lineW)/2,space+pos,lineW,lineH); 
+        let posX = (cw-lineW)/2;
+        ctx.fillRect(posX, posY, lineW, lineH); 
     }
 
 }
 
-function gameLoop() 
-{ 
-    drawTable();
+class Ball
+{
+    constructor()
+    {
+        this.d = 20;
+        this.x = cw/2 - this.d/2;
+        this.y = ch/2 - this.d/2;
+        this.speedX = 3;
+        this.speedY = 3;
+    }
 
-    ball.X += ball.speedX;
-    ball.Y += ball.speedY;
+    draw()
+    {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.x, this.y, this.d, this.d);
+    }
 
-    drawBall();
-}
+    move()
+    {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+};
 
+class Paddle
+{
+    constructor(x)
+    {
+        this.height = 100;
+        this.width = 10;
+        this.x = x;
+        this.y = ch/2 - this.height/2;
+    }
+
+    draw()
+    {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    movePlayer(e)
+    {
+        let y = e.clientY - canvasElem.offsetTop
+        let r = this.height/2;
+        if( y+r >= canvasElem.height)
+        {
+            this.y = canvasElem.height - this.height;
+        }
+        else if( y-r <= 0)
+        {
+            this.y = 0;
+        }
+        else
+        {
+            this.y = y - r;
+        }
+    }
+
+    moveAI()
+    {
+        this.y = ball.y - this.height/2;
+    }
+};
+
+
+setup();
 setInterval(gameLoop, 16);
